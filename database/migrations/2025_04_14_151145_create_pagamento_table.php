965617597922674acee46f18350afe33a4ac636d
Dorivaldo-Gonsalo <dorivaldogoncaloferreira011@gmail.com>
2025-04-14 16:12:45 +0100

    update migrate

---
+++ database/migrations/2025_04_14_151145_create_pagamento_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+class CreatePagamentoTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('pagamento', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('fatura_id')->notNull();
+            $table->date('data_pagamento')->notNull();
+            $table->decimal('valor_pago')->notNull();
+            $table->string('metodo_pagamento')->notNull();
+            $table->string('transacao_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('fatura_id')->references('id')->on('fatura')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('pagamento');
+    }
+}
