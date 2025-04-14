3251c52a973e89c0cd255280a5d6376aa5026a17
Dorivaldo-Gonsalo <dorivaldogoncaloferreira011@gmail.com>
2025-04-14 16:32:27 +0100

    update migrate

---
+++ database/migrations/2025_04_14_153145_create_voto_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+class CreateVotoTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('voto', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('votacao_id')->notNull();
+            $table->unsignedBigInteger('morador_id')->notNull();
+            $table->unsignedBigInteger('opcao_id')->notNull();
+            $table->dateTime('data_hora')->notNull();
+            $table->string('hash_voto', 255)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('votacao_id')->references('id')->on('votacao')->onDelete('cascade');
+            $table->foreign('morador_id')->references('id')->on('morador')->onDelete('cascade');
+            $table->foreign('opcao_id')->references('id')->on('opcao_votacao')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('voto');
+    }
+}
