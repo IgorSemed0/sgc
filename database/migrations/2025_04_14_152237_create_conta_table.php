d56bd3cc55f9c48a6ac8fb9152b67feae9722d37
Dorivaldo-Gonsalo <dorivaldogoncaloferreira011@gmail.com>
2025-04-14 16:23:47 +0100

    update migrate

---
+++ database/migrations/2025_04_14_152237_create_conta_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+class CreateContaTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('conta', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('condominio_id')->notNull();
+            $table->string('nome')->notNull();
+            $table->string('tipo')->notNull();
+            $table->decimal('saldo', 10, 2)->default(0.00);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('condominio_id')->references('condominio_id')->on('condominio')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('conta');
+    }
+}
