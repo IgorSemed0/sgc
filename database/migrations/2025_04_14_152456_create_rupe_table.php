413e4180ee7a7e920bb28294583e41c8890b28f6
Dorivaldo-Gonsalo <dorivaldogoncaloferreira011@gmail.com>
2025-04-14 16:25:44 +0100

    update migrate

---
+++ database/migrations/2025_04_14_152456_create_rupe_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+class CreateRupeTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('rupe', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('condominio_id')->notNull();
+            $table->string('descricao')->notNull();
+            $table->decimal('valor')->notNull();
+            $table->date('data_receita')->notNull();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('condominio_id')->references('condominio_id')->on('condominio')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('rupe');
+    }
+}
