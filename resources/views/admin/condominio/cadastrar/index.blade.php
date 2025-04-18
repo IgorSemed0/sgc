ab37cc8ee8d27b5ccc0f8cf94151ed6ff297b889
IgorSemed0 <semedoigor4@gmail.com>
2025-04-18 12:39:21 -0400

    feat(crud): condominio cadastrar viewfix bug

--- resources/views/admin/condominio/cadastrar/index.blade.php
+++ resources/views/admin/condominio/cadastrar/index.blade.php
@@ -1,6 +1,7 @@
 <div class="modal-body">
     <form action="{{ route('admin.condominio.store') }}" method="POST">
         @csrf
+        {{$condominio=null}}
         @include('admin._form.condominio.index')
         <button type="submit" class="btn btn-primary">Salvar</button>
     </form>
