68856113a4b33cb4eb05a4ff515b72532555e922
IgorSemed0 <semedoigor4@gmail.com>
2025-04-18 12:27:42 -0400

    feat(crud): condominio routes

--- resources/views/admin/associado/editar/index.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="modal-body">
-    <form action="{{ route('admin.associado.update', $associado->id) }}" method="POST">
-        @csrf
-        @method('PUT')
-        @include('admin._form.associado.index', ['associado' => $associado])
-        <button type="submit" class="btn btn-primary">Atualizar</button>
-    </form>
-</div>
--- routes/web.php
+++ routes/web.php
@@ -14,24 +14,19 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('admin.index');
     })->name('dashboard');
-    
-    
-    
+
     Route::prefix('dashboard')->middleware(['auth'])->group(function () {
-        // Users Routes
-        Route::prefix('users')->group(function () {
-            Route::get('index', ['as' => 'admin.users.index', 'uses' => 'App\Http\Controllers\Admin\UserController@index']);
-            Route::get('create', ['as' => 'admin.users.create', 'uses' => 'App\Http\Controllers\Admin\UserController@create']);
-            Route::post('store', ['as' => 'admin.users.store', 'uses' => 'App\Http\Controllers\Admin\UserController@store']);
-            Route::put('edit/{user}', ['as' => 'admin.users.edit', 'uses' => 'App\Http\Controllers\Admin\UserController@edit']);
-            Route::get('update/{user}', ['as' => 'admin.users.update', 'uses' => 'App\Http\Controllers\Admin\UserController@update']);
-            Route::get('show/{user}', ['as' => 'admin.users.show', 'uses' => 'App\Http\Controllers\Admin\UserController@show']);
-            Route::get('profile', ['as' => 'admin.users.profile', 'uses' => 'App\Http\Controllers\Admin\UserController@profile']);
-            Route::put('profile', ['as' => 'admin.users.profile.update', 'uses' => 'App\Http\Controllers\Admin\UserController@updateProfile']);
-            Route::get('trash', ['as' => 'admin.users.trash', 'uses' => 'App\Http\Controllers\Admin\UserController@trash']);
-            Route::delete('delete/{id}', ['as' => 'admin.users.delete', 'uses' => 'App\Http\Controllers\Admin\UserController@delete']);
-            Route::post('restore/{id}', ['as' => 'admin.users.restore', 'uses' => 'App\Http\Controllers\Admin\UserController@restore']);
-            Route::delete('purge/{id}', ['as' => 'admin.users.purge', 'uses' => 'App\Http\Controllers\Admin\UserController@purge']);
+        //Condominio Routes
+        Route::prefix('condominio')->group(function () {
+            Route::get('index', ['as' => 'admin.condominio.index', 'uses' => 'App\Http\Controllers\Admin\CondominioController@index']);
+            Route::get('create', ['as' => 'admin.condominio.create', 'uses' => 'App\Http\Controllers\Admin\CondominioController@create']);
+            Route::post('store', ['as' => 'admin.condominio.store', 'uses' => 'App\Http\Controllers\Admin\CondominioController@store']);
+            Route::get('edit/{id}', ['as' => 'admin.condominio.edit', 'uses' => 'App\Http\Controllers\Admin\CondominioController@edit']);
+            Route::put('update/{id}', ['as' => 'admin.condominio.update', 'uses' => 'App\Http\Controllers\Admin\CondominioController@update']);
+            Route::get('destroy/{id}', ['as' => 'admin.condominio.destroy', 'uses' => 'App\Http\Controllers\Admin\CondominioController@destroy']);
+            Route::get('trash', ['as' => 'admin.condominio.trash', 'uses' => 'App\Http\Controllers\Admin\CondominioController@trash']);
+            Route::post('restore/{id}', ['as' => 'admin.condominio.restore', 'uses' => 'App\Http\Controllers\Admin\CondominioController@restore']);
+            Route::delete('purge/{id}', ['as' => 'admin.condominio.purge', 'uses' => 'App\Http\Controllers\Admin\CondominioController@purge']);
         });
     });
 });
