238847772519cf8b1c4432d08a2446a9710ddce8
IgorSemed0 <semedoigor4@gmail.com>
2025-04-18 12:34:41 -0400

    feat(crud): condominio cadastrarview

--- resources/views/admin/condominio/cadastrar/index.blade.php
+++ resources/views/admin/condominio/cadastrar/index.blade.php
@@ -1,9 +1,7 @@
-@extends('admin.layouts.body')
-@section('title', 'Cadastrar Condomínio')
-@section('conteudo')
-<form action="{{ route('admin.condominio.store') }}" method="POST">
-    @csrf
-    @include('admin._form.condominio.index')
-    <button type="submit" class="btn btn-primary">Salvar</button>
-</form>
-@endsection
+<div class="modal-body">
+    <form action="{{ route('admin.condominio.store') }}" method="POST">
+        @csrf
+        @include('admin._form.condominio.index')
+        <button type="submit" class="btn btn-primary">Salvar</button>
+    </form>
+</div>
