f424e7e979f56a777f0bc85f37cf593fef5df40a
IgorSemed0 <semedoigor4@gmail.com>
2025-04-18 12:48:40 -0400

    feat(crud): bloco cadastrar view

--- resources/views/admin/bloco/cadastrar/index.blade.php
+++ resources/views/admin/bloco/cadastrar/index.blade.php
@@ -1,8 +1,8 @@
 <div class="modal-body">
-    <form action="{{ route('admin.condominio.store') }}" method="POST">
+    <form action="{{ route('admin.bloco.store') }}" method="POST">
         @csrf
-        {{$condominio=null}}
-        @include('admin._form.condominio.index')
+        {{$bloco=null}}
+        @include('admin._form.bloco.index')
         <button type="submit" class="btn btn-primary">Salvar</button>
     </form>
 </div>
