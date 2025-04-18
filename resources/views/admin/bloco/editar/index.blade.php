f63067519fd8101f683ea359677a74a272d3391d
IgorSemed0 <semedoigor4@gmail.com>
2025-04-18 12:48:23 -0400

    feat(crud): bloco editar view

--- resources/views/admin/bloco/editar/index.blade.php
+++ resources/views/admin/bloco/editar/index.blade.php
@@ -1,8 +1,8 @@
 <div class="modal-body">
-    <form action="{{ route('admin.condominio.update', $condominio->id) }}" method="POST">
+    <form action="{{ route('admin.bloco.update', $bloco->id) }}" method="POST">
         @csrf
         @method('PUT')
-        @include('admin._form.condominio.index', ['condominio' => $condominio])
+        @include('admin._form.bloco.index', ['bloco' => $bloco])
         <button type="submit" class="btn btn-primary">Atualizar</button>
     </form>
 </div>
