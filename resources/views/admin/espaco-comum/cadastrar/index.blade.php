f776cc4bf3ce8ae8100f0d8dbdb0777644a8a15f
IgorSemed0 <semedoigor4@gmail.com>
2025-04-20 09:58:31 -0400

    feat(crud): espaco_reservas

--- resources/views/admin/espaco-comum/cadastrar/index.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="modal-body">
-    <form action="{{ route('admin.espaco-comum.store') }}" method="POST">
-        @csrf
-        {{$espacoComum=null}}
-        @include('admin._form.espaco-comum.index')
-        <button type="submit" class="btn btn-primary">Salvar</button>
-    </form>
-</div>
--- resources/views/admin/espaco-comum/editar/index.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="modal-body">
-    <form action="{{ route('admin.voto.update', $voto->id) }}" method="POST">
-        @csrf
-        @method('PUT')
-        @include('admin._form.voto.index', ['voto' => $voto])
-        <button type="submit" class="btn btn-primary">Atualizar</button>
-    </form>
-</div>
