672a1a0f0c9a5f36b0413daac3ce2fe5b36bc510
IgorSemed0 <semedoigor4@gmail.com>
2025-05-07 08:00:20 +0100

    fix(issue): 7
    add types and nullables to fields

--- database/migrations/2025_04_14_115545_create_moradors_table.php
+++ database/migrations/2025_04_14_115545_create_moradors_table.php
@@ -9,22 +9,25 @@ return new class extends Migration
     public function up()
     {
         Schema::create('moradors', function (Blueprint $table) {
-            $table->id(); 
+            $table->id();
             $table->string('primeiro_nome');
             $table->string('nomes_meio')->nullable();
             $table->string('ultimo_nome');
             $table->string('email');
             $table->string('username')->nullable();
-            $table->string('processo');
             $table->string('tipo');
+            $table->boolean('estado_residente')->default(false);
             $table->string('telefone');
-            $table->string('bi');
+            $table->string('bi')->nullable();
+            $table->string('cedula')->nullable();
             $table->date('data_nascimento');
             $table->string('sexo');
-            $table->unsignedBigInteger('unidade_id'); 
-            $table->timestamps(); 
+            $table->unsignedBigInteger('unidade_id');
+            $table->unsignedBigInteger('dependente_de')->nullable();
+            $table->timestamps();
             $table->softDeletes();
             $table->foreign('unidade_id')->references('id')->on('unidades')->onDelete('cascade');
+            $table->foreign('dependente_de')->references('id')->on('moradors')->onDelete('set null');
         });
     }
 
