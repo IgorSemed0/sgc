14daab58ff4095e5f059258e1c4ccdbf1cf1c4e2
IgorSemed0 <semedoigor4@gmail.com>
2025-05-07 15:03:03 +0100

    feat(home): charts update

--- app/Http/Controllers/Admin/PagamentoController.php
+++ app/Http/Controllers/Admin/PagamentoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Pagamento;
+use App\Models\Conta;
 use App\Models\Factura;
 use Illuminate\Http\Request;
 
